a85b23d99ba2e47b634cad4931173de51645d903
NouranMaged <nouran.maged.a@gmail.com>
2023-03-17 14:54:00 -0700

    dsfds

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,32 +1,3 @@
-
-Skip to content
-Pull requests
-Issues
-Codespaces
-Marketplace
-Explore
-@NouranMaged
-ianliu-johnston /
-holbertonschool-low_level_programming
-Public
-
-Fork your own copy of ianliu-johnston/holbertonschool-low_level_programming
-
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-
-    Insights
-
-holbertonschool-low_level_programming/0x01-variables_if_else_while/0-positive_or_negative.c
-Ian Liu-Johnston Added task 0 and aliases.
-Latest commit 736bc22 Oct 13, 2016
-History
-0 contributors
-30 lines (28 sloc) 443 Bytes
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -57,20 +28,3 @@ int main(void)
 	return (0);
 
 }
-Footer
-© 2023 GitHub, Inc.
-Footer navigation
-
-    Terms
-    Privacy
-    Security
-    Status
-    Docs
-    Contact GitHub
-    Pricing
-    API
-    Training
-    Blog
-    About
-
-
